0ba029cf900e53274b9b0d91e1cc434903d0acd8
mrloureed <mrloureed@gmail.com>
2025-11-03 12:29:11 -0500

    add port back

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
+    open: false,
     proxy: {
       '/api': 'http://localhost:3001',
     },
   },
   base: "./",
   build: {
-    outDir: "module/apps/dist",
-  },
-})
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
+});
