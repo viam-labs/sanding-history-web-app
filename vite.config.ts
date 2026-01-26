4017de2e7ae05df8e36d15f7a9a2007b52c41ce2
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2026-01-26 09:25:23 -0500

    add command to view bundle

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
+import { analyzer } from 'vite-bundle-analyzer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svelte(), tailwindcss()],
+  plugins: [react(), svelte(), tailwindcss(), analyzer()],
   server: {
     port: 3000,
     open: false
